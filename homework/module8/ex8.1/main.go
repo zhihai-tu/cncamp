8524d77f5f1d9fa4105c18c6c9b4035840728a95
zhihai-tu <tu_zh@163.com>
2022-04-10 21:21:42 +0800

    config log level

--- homework/module8/ex8.1/main.go
+++ homework/module8/ex8.1/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
-	"log"
+	"io"
 	"net"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"runtime"
+	"strings"
 
-	"net/http/pprof"
+	"github.com/golang/glog"
 )
 
 func main() {
+
+	glog.MaxSize = 1024 * 1024 * 20 // 20M自动分割
+	//读取配置文件，设置glog的level
+	config := InitConfig("app.properties")
+	loglevel := config["loglevel"]
+	logpath := config["logpath"]
+
+	flag.Set("log_dir", logpath)
+	flag.Set("alsologtostderr", "true")
+	flag.Set("v", loglevel)
+	flag.Parse()
+	defer glog.Flush()
+
+	pathFlag, pathErr := PathExists(logpath)
+	if !pathFlag {
+		glog.Error(pathErr)
+	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", rootHandler)
@@ -23,19 +44,22 @@ func main() {
 
 	//根据环境变量获取访问端口，默认为80
 	httpport := os.Getenv("HTTP_PORT")
-	fmt.Printf("GET ENV: HTTP_PORT=%s", httpport)
+	//fmt.Printf("GET ENV: HTTP_PORT=%s", httpport)
+	glog.V(5).Infof("GET ENV: HTTP_PORT=%s", httpport)
 	if httpport == "" {
 		httpport = "80"
 	}
 	err := http.ListenAndServe(":"+httpport, mux)
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
+		glog.Fatal(err)
 	}
 }
 
 //4、当访问 localhost/healthz 时，应返回 200
 func healthz(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "system is working... httpcode: %d \n", 200)
+	fmt.Fprintf(w, "<html h1>system is working... httpcode: %d </html>", 200)
+	glog.V(3).Infof("system is working... httpcode: %d \n", 200)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,7 +67,8 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	//1、接收客户端 request，并将 request 中带的 header 写入 response header
 	for k, v := range r.Header {
 		for _, vv := range v {
-			fmt.Printf("Header key: %s, value: %s \n", k, vv)
+			//fmt.Printf("Header key: %s, value: %s \n", k, vv)
+			glog.V(5).Infof("Header key: %s, value: %s \n", k, vv)
 			w.Header().Set(k, vv)
 		}
 	}
@@ -53,14 +78,20 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	//version := os.Getenv("GOVERSION") //TODO:为什么不行？
 	//path := os.Getenv("GOPATH")
 	//fmt.Printf("PATH: %s \n", path)	//OK
-	fmt.Printf("VERSION: %s \n", version)
+	//fmt.Printf("VERSION: %s \n", version)
+	glog.V(5).Infof("VERSION: %s \n", version)
 	w.Header().Set("version", version)
 
 	//3、Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
 	clientIp := RemoteIp(r)
 	retCode := 200
-	log.Printf("Clientip: %s \n", clientIp)
-	log.Printf("Http Return Code: %d", retCode)
+	//log.Printf("Clientip: %s \n", clientIp)
+	glog.V(5).Infof("Clientip: %s \n", clientIp)
+	//log.Printf("Http Return Code: %d", retCode)
+	glog.V(5).Infof("Http Return Code: %d", retCode)
+
+	fmt.Fprint(w, "<html h1>Welcome to cncamp...</html>")
+	glog.V(3).Info("Welcome to cncamp...")
 }
 
 func RemoteIp(req *http.Request) string {
@@ -72,10 +103,65 @@ func RemoteIp(req *http.Request) string {
 	} else {
 		remoteAddr, _, _ = net.SplitHostPort(remoteAddr)
 	}
-
 	if remoteAddr == "::1" {
 		remoteAddr = "127.0.0.1"
 	}
-
 	return remoteAddr
+}
+
+//读取key=value类型的配置文件
+func InitConfig(path string) map[string]string {
+	config := make(map[string]string)
+
+	f, err := os.Open(path)
+	if err != nil {
+		defer f.Close()
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	r := bufio.NewReader(f)
+	for {
+		b, _, err := r.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		s := strings.TrimSpace(string(b))
+		index := strings.Index(s, "=")
+		if index < 0 {
+			continue
+		}
+		key := strings.TrimSpace(s[:index])
+		if len(key) == 0 {
+			continue
+		}
+		value := strings.TrimSpace(s[index+1:])
+		if len(value) == 0 {
+			continue
+		}
+		config[key] = value
+	}
+	return config
+}
+
+//PathExists 判断文件夹是否存在,不存在就创建
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		// 创建文件夹
+		err := os.MkdirAll(path, os.ModePerm)
+		if err != nil {
+			fmt.Printf("mkdir failed![%v]\n", err)
+		} else {
+			return true, nil
+		}
+	}
+	return false, err
 }
